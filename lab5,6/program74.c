3fb451af02590d709c3e2a4ecbc77f72fc2dbbb1
Manav-020 <u24ai020@aid.svnit.ac.in>
2024-11-19 08:33:07 +0530

    Update program74.c

--- lab5,6/program74.c
+++ lab5,6/program74.c
@@ -13,55 +13,37 @@ int main() {
     for(int i=0;i<5;i++){
         scanf("%d",&B[i]);
     }
+
     for(int i=0;i<5;i++){
         printf("\t%d",B[i]);
     }
     
-    for(int i=0;i<5;i++)
-    {  
-        for(int j=0;j<10-i;j++){
-        temp=A[j];
-        if(temp>A[j+1]){
-            A[j]=A[j+1];
-            A[j+1]=temp;
-            }
-        }
-    }
-    for(int i=0;i<5;i++)
-    {  
-        for(int j=0;j<10-i;j++){
-        temp=B[j];
-        if(temp>B[j+1]){
-            B[j]=B[j+1];
-            B[j+1]=temp;
-            }
-        }
-    }
-    // printf("\narray A in asecending order is\n");
-    // for(int i=0;i<5;i++){
-    //     printf("\t%d",A[i]);
-    // }
-    // printf("\narray B in asecending order is\n");
-    // for(int i=0;i<5;i++){
-    //     printf("\t%d",A[i]);
-    // }
-    for(int i=0,j=5;j<10,i<5;i++,j++)
+    for(int i=0,j=5;i<5,j<10;i++,j++)
     {
-        if(A[0]>B[4])
-        {
-            C[i]=B[i];
-            C[j]=A[i];
-        }
-        else
-        {
-             C[i]=B[j];
-            C[j]=A[i];
-        }
-    }
-    printf("\narray C in asecending order is\n");
-    for(int i=0;i<10;i++){
-        printf("\t%d",C[i]);
+        C[i]=A[i];
+        C[j]=B[i];
     }
 
+
+
+    for(int i=0;i<10;i++)
+    {  
+        for(int j=0;j<10-i;j++){
+        temp=C[j];
+        if(temp>C[j+1]){
+            C[j]=C[j+1];
+            C[j+1]=temp;
+            }
+        }
+    }
+    
+    printf("\ncombination of sorted array: \n");
+    for(int i=0;i<10;i++){
+        printf("%d\t",C[i]);
+    }
+
+    
+
     return 0;
+}
 }
